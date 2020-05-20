8572b026c69fc3027537247010e4e2ae0f808ee9
alageev <alageev5610@gmail.com>
2020-05-20 09:47:44 +0300

    Modified slider and added smooth scroll

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,26 @@
 $(document).ready(function () {
+  /* Slick slider */
   $('.slider').slick({
     arrows: false,
     dots: true,
-    speed: 1500,
     fade: true,
+    waitForAnimate: false,
+    speed: 1250,
     initialSlide: 1,
+  });
+
+  /* Smooth scroll */
+  $('[data-scroll]').on('click', function (event) {
+    event.preventDefault();
+
+    let elementClass = $(this).data('scroll');
+    let elementOffset = $(elementClass).offset().top;
+
+    $('html, body').animate(
+      {
+        scrollTop: elementOffset,
+      },
+      1000
+    );
   });
 });
