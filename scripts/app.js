ab82d3debeb09a382022a5c9497275ed7299323b
alageev <alageev5610@gmail.com>
2020-05-20 17:34:26 +0300

    Added pop-up

--- scripts/app.js
+++ scripts/app.js
@@ -38,4 +38,24 @@ $(document).ready(function () {
       1000
     );
   });
+
+  /* Fancybox */
+});
+
+$(document).ready(function () {
+  $('.fancybox').fancybox();
+});
+
+$(document).ready(function () {
+  $('.various').fancybox({
+    maxWidth: 800,
+    maxHeight: 600,
+    fitToView: false,
+    width: '70%',
+    height: '70%',
+    autoSize: false,
+    closeClick: false,
+    openEffect: 'elastic',
+    closeEffect: 'none',
+  });
 });
